66f9732cc104986d733ceb3d5849aa9578175464
220003 <220003pb@yse-c.net>
2023-11-17 11:30:17 +0900

    second

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>商品情報</h2>
+<x-app-layout>
+    <h2>Item detail</h2>
+    <h3>ID</h3>
+    <p>{{ $id }}</p>
     <h3>商品名</h3>
-    <p>
-        {{ $item }}
-    </p>
-</body>
-</html>
+    <p>{{ $item }}</p>
+</x-app-layout>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>検索結果</h2>
-    <h3>検索キーワード</h3>
-    <!-- これで変数を持ってこれる -->
-    <p>{{$keyword}}</p>
-</body>
-</html>
+<x-app-layout>
+    <h2>Search</h2>
+    <form action="/search" method="get">
+        <input type="text" name="q" value="{{ $keyword }}">
+        <button>Search</button>
+    </form>
+    <h3>{{ $keyword }}の検索結果</h3>
+    <div>
+        .....
+    </div>
+</x-app-layout>
