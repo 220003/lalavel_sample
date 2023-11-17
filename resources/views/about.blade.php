3ca1bd52de3fffb9a7a926a6db7d88d122d19477
220003 <220003pb@yse-c.net>
2023-11-17 11:23:36 +0900

    lalavel

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>About</h2>
-    <div>
-        This is About Page
-    </div>    
-</body>
-</html>
+<x-app-layout>
+    <h2>About Page</h2>
+    <p>This is about page</p>
+</x-app-layout>
