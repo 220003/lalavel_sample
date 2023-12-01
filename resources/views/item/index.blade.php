14b9022a66a00c7b11ead156395cc31f338a5a63
220003 <220003pb@yse-c.net>
2023-12-01 11:25:22 +0900

    first

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -1,15 +1,26 @@
 <x-app-layout>
-    <h2>Item List</h2>
-
-    <a href="{{ route('item.create') }}">New</a>
-    <table>
-        @foreach($items as $item)
-        <tr>
-            <td>
-                <a href="{{ route('item.show', $item->id) }}">{{ $item->name }}</a>
-            </td>
-            <td>{{ $item->price }}円</td>
-        </tr>
-        @endforeach
-    </table>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h2>商品一覧</h2>
+                    <a href="{{ route('item.create') }}">新規追加</a>
+                    <table class="tabel">
+                        <tr>
+                            <th></th>
+                            <th>商品名</th>
+                            <th>価格</th>
+                        </tr>
+                        @foreach($items as $item)
+                        <tr>
+                            <td><a href="{{ route('item.edit', $item->id) }}">編集</a></td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->price }}</td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,25 @@
 <?php
-
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // HomeController
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/search', [HomeController::class, 'search'])->name('search');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 // ItemController
 Route::get('/item/', [ItemController::class, 'index'])->name('item.index');
-Route::get('/item/create', [ItemController::class,'create'])->name('item.create');
-Route::post('/item/store', [ItemController::class,'store'])->name('item.store');
+Route::get('/item/create', [ItemController::class, 'create'])->name('item.create');
+Route::post('/item/store', [ItemController::class, 'store'])->name('item.store');
 Route::get('/item/{id}', [ItemController::class, 'show'])->name('item.show');
+Route::get('/item/edit/{id}', [ItemController::class, 'edit'])->name('item.edit');
+Route::post('/item/updqte/{id}', [ItemController::class, 'update'])->name('item.update');
+
+Route::get('/', function () {
+    // resources/views/welcome.blade.php ビューが表示
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -31,4 +31,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
